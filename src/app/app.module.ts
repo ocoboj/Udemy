7d785d6e77d0f58f98c1061b34de4b4b56a8f1d1
Olga Conesa <olgaconesaboj@gmail.com>
2019-07-23 13:05:56 +0200

    creando la pagina de busqueda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ItemComponent } from './pages/item/item.component';
 // Rutas
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    ItemComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,

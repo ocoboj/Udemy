ffe5f5e55cedf8774f0c3b3f922b5f5f217c79bd
Olga Conesa <olgaconesaboj@gmail.com>
2019-07-23 14:04:20 +0200

    diseño y filtro de la pagina de busqueda

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public productoService: ProductosService) { }
 
   ngOnInit() {
+    this.route.params
+    .subscribe(params => {
+      console.log(params['termino']);
+      this.productoService.buscarProducto(params['termino']);
+
+    });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,26 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
+
   constructor(private http: HttpClient) {
     this.cargarPrdouctos();
   }
 
   private cargarPrdouctos() {
-    this.http.get('https://angular-html-4da87.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
+    this.http.get('https://angular-html-4da87.firebaseio.com/productos_idx.json').subscribe((resp: Producto[]) => {
       this.productos = resp;
       this.cargando = false;
     });
   }
 
-  getProducto( id: string ) {
-   return this.http.get(`https://angular-html-4da87.firebaseio.com/productos/${id}.json`);
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-4da87.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string) {
+    this.productosFiltrado = this.productos.filter(producto => {
+      return true;
+    });
+    console.log( this.productosFiltrado );
   }
 }
